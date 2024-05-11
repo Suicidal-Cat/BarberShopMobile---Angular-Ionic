b1af6b1a4cfc9b0c69ac6a157c6b00cdea4952e6
Suicidal-Cat <darekolaric@gmail.com>
2024-05-11 15:18:13 +0200

    added jwt intercepter

--- src/app/interceptors/jwt.interceptor.ts
+++ src/app/interceptors/jwt.interceptor.ts
@@ -1,5 +1,28 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AccountService } from '../services/Account/account.service';
 
-export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
-};
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+
+  constructor(private accountService:AccountService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+
+    const jwt=this.accountService.getJWT();
+    if(jwt){
+      request=request.clone({
+        setHeaders:{
+          Authorization: `Bearer ${jwt}`
+        }
+      });
+    }
+    return next.handle(request);
+  }
+}
