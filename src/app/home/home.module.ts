9bc65e609b3b9102d0ce0d1868672b251c1e960f
Suicidal-Cat <darekolaric@gmail.com>
2024-06-30 23:42:48 +0200

    added tabs for admin and toolbar

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { ToolbarComponent } from './util-components/toolbar/toolbar.component';
 
 
 @NgModule({
@@ -14,6 +15,6 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage,ToolbarComponent]
 })
 export class HomePageModule {}
