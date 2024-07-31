a30728864d5a79db96a9574b6b6c29f899055b24
Suicidal-Cat <darekolaric@gmail.com>
2024-08-01 00:21:33 +0200

    added previous and next days in month calendar

--- src/app/home/reservations-page/reservation-calendar/reservation-calendar.component.ts
+++ src/app/home/reservations-page/reservation-calendar/reservation-calendar.component.ts
@@ -10,12 +10,15 @@ export class ReservationCalendarComponent  implements OnInit {
   startMonth:Date=new Date();
   startDay:number=new Date().getDate();
   selectedDay:number=this.startDay;
-  selectedWeek!:number;
+  selectedMonthForDay:Date=this.startMonth;
+  selectedWeek:number=-1;
   showSelectedWeek:boolean=true;
 
   currentMonth!:Date;
   daysInMonth: number[][]=[];
   weekDays:String[] = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  startColumnOfMonth!:number;
+  endColumnOfMonth!:number;
 
   @Output() selectedDateUser:EventEmitter<string>=new EventEmitter();
 
@@ -35,19 +38,20 @@ export class ReservationCalendarComponent  implements OnInit {
   getAppointments(day:number){
     const date=new Date(this.currentMonth.getFullYear(),this.currentMonth.getMonth(),day);
     this.selectedDay=day;
+    this.selectedMonthForDay=date;
     this.selectedDateUser.emit(this.formatDate(date));
   }
 
   nextMonth(){
     this.currentMonth = new Date(this.currentMonth.getFullYear(),this.currentMonth.getMonth()+1);
     if(this.startMonth.getMonth()==this.currentMonth.getMonth()){
-      this.selectedDay=this.startDay;
-      this.selectedDateUser.emit(this.formatDate(this.startMonth))
+      // this.selectedDay=this.startDay;
+      // this.selectedDateUser.emit(this.formatDate(this.startMonth))
     }
-    else {
-      this.selectedDay=-1;
-      this.selectedWeek=0;
-    }
+    // else {
+    //   this.selectedDay=-1;
+    //   this.selectedWeek=0;
+    // }
     this.generateCalendar(this.currentMonth);
   }
 
@@ -55,13 +59,14 @@ export class ReservationCalendarComponent  implements OnInit {
     this.currentMonth = new Date(this.currentMonth.getFullYear(),this.currentMonth.getMonth()-1);
     if(this.startMonth.getMonth()==this.currentMonth.getMonth()){
       this.currentMonth = new Date(this.currentMonth.getFullYear(),this.currentMonth.getMonth(),this.startDay);
-      this.selectedDay=this.startDay;
-      this.selectedDateUser.emit(this.formatDate(this.startMonth))
+      // this.selectedDay=this.startDay;
+      // this.selectedDateUser.emit(this.formatDate(this.startMonth))
     }
-    else {
-      this.selectedDay=-1;
-      this.selectedWeek=0;
-    }
+    // else {
+    //   this.selectedDay=-1;
+    //   this.selectedWeek=0;
+    // }
+    this.selectedWeek=0;
     this.generateCalendar(this.currentMonth);
   }
 
@@ -92,7 +97,14 @@ export class ReservationCalendarComponent  implements OnInit {
 
     daysInMonth[0]=[];
 
-    for(let i=0;i<startDayOfTheWeek;i++)daysInMonth[0][i]=0;
+    let dayOfPreviosMonth=new Date(start.getFullYear(),start.getMonth(),0).getDate();
+    
+    for(let i=startDayOfTheWeek-1;i>=0;i--){
+      daysInMonth[0][i]=dayOfPreviosMonth;
+      dayOfPreviosMonth--;
+    }
+
+    this.startColumnOfMonth=startDayOfTheWeek;
     for(let i=startDayOfTheWeek;i<7;i++){
       daysInMonth[0][i]=currentDay;
       currentDay++;
@@ -109,19 +121,42 @@ export class ReservationCalendarComponent  implements OnInit {
       }
     }
 
-    for(let i=endDayOfTheWeek+1;i<7;i++)daysInMonth[row][i]=0;
+    let nextMonthDays=1;
+    for(let i=endDayOfTheWeek+1;i<7;i++){
+      daysInMonth[row][i]=nextMonthDays;
+      nextMonthDays++;
+    }
+    this.endColumnOfMonth=endDayOfTheWeek;
+    if(start.getMonth()==this.startMonth.getMonth() && this.selectedWeek==-1){
+      for(let i=0;i<=row;i++){
+        for(let j=0;j<7;j++){
+          if(daysInMonth[i][j]==this.startDay){
+            this.selectedWeek=i;
+            i=row+1;
+            j=7;
+          }
+        }
+      }    
+    }
 
-    if(start.getMonth()==this.startMonth.getMonth())
-      for(let i=0;i<=row;i++)
-        for(let j=0;j<7;j++)if(daysInMonth[i][j]==this.startDay){
-          this.selectedWeek=i;
-          break;
-      }
     return daysInMonth;
   }
 
   showHideCal(){
     this.showSelectedWeek=!this.showSelectedWeek;
+  }
+
+  startDayClass(day:number,i:number,j:number):boolean{
+    let t= day==this.selectedDay && this.selectedMonthForDay.getMonth()==this.currentMonth.getMonth() 
+    && !((i==1 && j<this.startColumnOfMonth) || (i==this.daysInMonth.length-1 && j>this.endColumnOfMonth));
+
+    if(t){
+      // console.log(day,i,j,'PRESEK',this.startColumnOfMonth,this.endColumnOfMonth)
+      // console.log(this.selectedMonthForDay.getMonth()==this.currentMonth.getMonth())
+      // console.log(!((i==1 && j<this.startColumnOfMonth) || (i==this.daysInMonth.length-1 && j>this.endColumnOfMonth)))
+    }
+
+    return t;
   }
 
 }
